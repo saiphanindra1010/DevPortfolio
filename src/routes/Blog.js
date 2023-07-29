ff88cd205b05d4c8a9438fff27caaecfc06da752
saiphanindra1010 <saiphanindra1010@gmail.com>
2023-07-29 19:27:59 +0530

    pages

---
+++ src/routes/Blog.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navbar } from '../components/Navbar'
+import { Bloggrid } from '../components/Bloggrid'
+
+export const Blog = () => {
+  return (
+    <div>
+        {/* <Navbar/> */}
+        <Bloggrid/>
+</div>
+  )
+}
