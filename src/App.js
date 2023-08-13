dba5fafea1960daaff7c4ab15bad4f5696e05956
saiphanindra1010 <saiphanindra1010@gmail.com>
2023-08-13 17:54:56 +0530

    css fix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import "./index.css"
+import './index.css'
+import Home from "./routes/Home"
+import Project from "./routes/Project"
+// import Contact from "./routes/Contact"
+import Contact from "./routes/Contact";
+import { Blog } from "./routes/Blog";
+import { Route, Routes } from "react-router-dom";
+import { Notfoundpage } from "./routes/Notfoundpage";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+<Routes>
+<Route path="/" element={<Home/>}></Route>
+<Route path="/projects" element={<Project/>}></Route>
+<Route path="/blogs" element={<Blog/>}></Route>
+<Route path="/contacts" element={<Contact/>}></Route>
+<Route path="*" element={<Notfoundpage/>}></Route>
+</Routes>
+  </>
   );
 }
 
